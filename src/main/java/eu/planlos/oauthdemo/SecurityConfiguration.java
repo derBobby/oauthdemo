79b86d2a3f2434ba035d436004a06a387606d122
derBobby <dev@planlos.eu>
2024-03-26 23:33:32 +0100

    Add Config

--- src/main/java/eu/planlos/oauthdemo/SecurityConfiguration.java
+++ src/main/java/eu/planlos/oauthdemo/SecurityConfiguration.java
@@ -6,8 +6,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
@@ -18,8 +16,7 @@ public class SecurityConfiguration {
                 .authorizeHttpRequests((authz) -> authz
                         .requestMatchers("/oauth").hasAuthority("OIDC_USER")
                         .anyRequest().permitAll()
-                )
-                .httpBasic(withDefaults());
+                );
 
         return http.build();
     }
