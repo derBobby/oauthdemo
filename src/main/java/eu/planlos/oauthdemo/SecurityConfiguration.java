80317ff5aed3517a447ecfc26033ca8c908e1b2e
derBobby <dev@planlos.eu>
2024-03-26 23:19:33 +0100

    Add Config

---
+++ src/main/java/eu/planlos/oauthdemo/SecurityConfiguration.java
@@ -0,0 +1,26 @@
+package eu.planlos.oauthdemo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .requestMatchers("/oauth").hasAuthority("OIDC_USER")
+                        .anyRequest().permitAll()
+                )
+                .httpBasic(withDefaults());
+
+        return http.build();
+    }
+}
