a01a4dc488736d49e1826583022177b1c0e30d97
derBobby <dev@planlos.eu>
2024-04-03 00:36:16 +0200

    Test run

--- src/test/java/eu/planlos/oauthdemo/OauthdemoApplicationTests.java
+++ src/test/java/eu/planlos/oauthdemo/OauthdemoApplicationTests.java
@@ -2,8 +2,10 @@ package eu.planlos.oauthdemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(classes = {TestSecurityConfiguration.class})
 class OauthdemoApplicationTests {
 
     @Test
