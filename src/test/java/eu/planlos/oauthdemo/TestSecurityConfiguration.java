d25222f3ebf0989441d9f23996c725a248d5b2b7
derBobby <dev@planlos.eu>
2024-04-04 01:09:54 +0200

    Fix health endpoint - remove oauth

--- src/test/java/eu/planlos/oauthdemo/TestSecurityConfiguration.java
+++ src/test/java/eu/planlos/oauthdemo/TestSecurityConfiguration.java
@@ -1,15 +1,15 @@
-package eu.planlos.oauthdemo;
-
-import org.mockito.Mockito;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-
-@TestConfiguration
-public class TestSecurityConfiguration {
-
-    @Bean
-    public ClientRegistrationRepository clientRegistrationRepository() {
-        return Mockito.mock(ClientRegistrationRepository.class);
-    }
-}
+//package eu.planlos.oauthdemo;
+//
+//import org.mockito.Mockito;
+//import org.springframework.boot.test.context.TestConfiguration;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+//
+//@TestConfiguration
+//public class TestSecurityConfiguration {
+//
+//    @Bean
+//    public ClientRegistrationRepository clientRegistrationRepository() {
+//        return Mockito.mock(ClientRegistrationRepository.class);
+//    }
+//}
